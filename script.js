570ef09e3bb98ad1b0cd7a4d5103b240efa5c47d
pongnatel <s3877819@rmit.edu.vn>
2023-11-27 09:18:05 +0700

    Fix 60 times

--- script.js
+++ script.js
@@ -5,19 +5,19 @@ var clicks = 0;
 var attempts = 0;
 var times = 0;
 var degreeMapping = {
-  one: "Orange",
-  two: "Yellow",
-  three: "Dark Blue",
-  four: "Blue",
-  five: "Green",
-  six: "Red",
+  one: "BIGGEST PRIZE: LANDYARDDDDD",
+  two: "Wow you win a loofah!",
+  three: "Arg, I also want a cup holder. Take yours",
+  four: "Greenmart's pen is waiting for you!!!",
+  five: "You have one more spin",
+  six: "Better luck next time",
 };
 var big = false;
 
 let scam = [
-  3.5, 3.7, 3.1, 3.7, 1.2, 0, 3.8, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
-  0.1, 2.3, 1.25, 1.45, 3.5, 3.7, 3.1, 3.7, 1.2, 0, 3.8, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
-  0.1, 2.3, 1.25, 1.45, 3.5, 3.7, 3.1, 3.7, 1.2, 0, 3.8, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
+  3.5, 0.1, 3.1, 0.1, 1.2, 0, 0.2, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
+  0.1, 2.3, 1.25, 1.45, 3.5, 0.1, 3.1, 0.1, 1.2, 0, 0.2, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 0.2, 0, 3.2,
+  0.1, 2.3, 1.25, 1.45, 3.5, 0.1, 3.1, 0.1, 1.2, 0, 0.2, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
   0.1, 2.3, 1.25, 1.45
 ];
 let values = [
@@ -140,28 +140,41 @@ $(document).ready(function () {
         $("#inner-wheel").on(
           "transitionend webkitTransitionEnd oTransitionEnd",
           function () {
-              switch (values[attempts - 1]) {
-                case "red":
-                  $("#winnings").text("Better luck next time");
-                  // $("#winnings").text("BIGGEST PRIZE: LANDYARDDDDD");
-                  break;
-                case "yellow":
-                  $("#winnings").text("Wow you win a loofah!");
-                  break;
-                case "blue":
-                  $("#winnings").text("Greenmart's pen is waiting for you!!!");
-                  break;
-                case "green":
-                  $("#winnings").text("You have one more spin");
-                  break;
-                case "dark blue":
-                  $("#winnings").text(
-                    "Arg, I also want a cup holder. Take yours"
-                  );
-                  break;
-                default:
-                  $("#winnings").text("Better luck next time"); // fix bug
+            if (extraDegree > 30 && extraDegree <= 90) {
+              $("#winnings").text(`${degreeMapping.one}`);
+              } else if (extraDegree > 90 && extraDegree <= 150) {
+              $("#winnings").text(`${degreeMapping.two}`);
+              } else if (extraDegree > 150 && extraDegree <= 210) {
+              $("#winnings").text(`${degreeMapping.three}`);
+              } else if (extraDegree > 210 && extraDegree <= 270) {
+              $("#winnings").text(`${degreeMapping.four}`);
+              } else if (extraDegree > 270 && extraDegree <= 330) {
+              $("#winnings").text(`${degreeMapping.five}`);
+              } else if(extraDegree>=330 || extraDegree <= 30){
+                $("#winnings").text(`${degreeMapping.six}`);
               }
+              // switch (values[attempts - 1]) {
+              //   case "red":
+              //     $("#winnings").text("Better luck next time");
+              //     // $("#winnings").text("BIGGEST PRIZE: LANDYARDDDDD");
+              //     break;
+              //   case "yellow":
+              //     $("#winnings").text("Wow you win a loofah!");
+              //     break;
+              //   case "blue":
+              //     $("#winnings").text("Greenmart's pen is waiting for you!!!");
+              //     break;
+              //   case "green":
+              //     $("#winnings").text("You have one more spin");
+              //     break;
+              //   case "dark blue":
+              //     $("#winnings").text(
+              //       "Arg, I also want a cup holder. Take yours"
+              //     );
+              //     break;
+              //   default:
+              //     $("#winnings").text("Better luck next time"); // fix bug
+              // }
             }
         );
 
