729f6603590c8d2d851f18ad64113041c339cc5f
pongnatel <s3877819@rmit.edu.vn>
2023-11-27 07:54:27 +0700

    draft 60 times

--- script.js
+++ script.js
@@ -12,12 +12,55 @@ var degreeMapping = {
   five: "Green",
   six: "Red",
 };
+var big = false;
 
 let scam = [
   3.5, 3.7, 3.1, 3.7, 1.2, 0, 3.8, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
-  0.1, 2.3, 1.25, 1.45,
+  0.1, 2.3, 1.25, 1.45, 3.5, 3.7, 3.1, 3.7, 1.2, 0, 3.8, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
+  0.1, 2.3, 1.25, 1.45, 3.5, 3.7, 3.1, 3.7, 1.2, 0, 3.8, 1.3, 0.95, 2.8, 0.1, 2, 1.2, 3.8, 0, 3.2,
+  0.1, 2.3, 1.25, 1.45
 ];
 let values = [
+  "red",
+  "red",
+  "green",
+  "red",
+  "yellow",
+  "red",
+  "red",
+  "yellow",
+  "yellow",
+  "green",
+  "red",
+  "dark blue",
+  "yellow",
+  "red",
+  "red",
+  "green",
+  "red",
+  "blue",
+  "yellow",
+  "yellow",
+  "red",
+  "red",
+  "green",
+  "red",
+  "yellow",
+  "red",
+  "red",
+  "yellow",
+  "yellow",
+  "green",
+  "red",
+  "dark blue",
+  "yellow",
+  "red",
+  "red",
+  "green",
+  "red",
+  "blue",
+  "yellow",
+  "yellow",
   "red",
   "red",
   "green",
@@ -42,7 +85,7 @@ let values = [
 
 $(document).ready(function () {
   /*WHEEL SPIN FUNCTION*/
-  if (attempts < 20) {
+  if (attempts < 60) {
     $("#spin").click(function () {
       $("#winnings").text("");
       //var extraDegree = [Math.floor(scam[attempts])];
@@ -52,9 +95,9 @@ $(document).ready(function () {
       clicks++;
       attempts++;
 
-      if (times == 3) {
-        extraDegree = 3.95;
-      }
+      // if (times == 1) {
+      //   extraDegree = 395;
+      // }
 
       var newDegree = degree * clicks;
       totalDegree = newDegree + extraDegree;
@@ -97,13 +140,6 @@ $(document).ready(function () {
         $("#inner-wheel").on(
           "transitionend webkitTransitionEnd oTransitionEnd",
           function () {
-            if (times == 3) {
-              $("#winnings").text("BIGGEST PRIZE: LANDYARDDDDD");
-              attempts = 0;
-              times = 0;
-            } else {
-              console.log("attemps before: " + attempts);
-
               switch (values[attempts - 1]) {
                 case "red":
                   $("#winnings").text("Better luck next time");
@@ -124,22 +160,104 @@ $(document).ready(function () {
                   );
                   break;
                 default:
-                  $("#winnings").text("Wow you win a loofah!"); // fix bug
+                  $("#winnings").text("Better luck next time"); // fix bug
               }
             }
-          }
         );
 
         noY = t.offset().top;
-        if (attempts == 20) {
-          attempts = 0;
-          times++;
-        }
-        console.log("attemps: " + attempts);
-        console.log("times: " + times);
+        
       });
-      
+
+      console.log("attemps: " + attempts);
+      console.log("times: " + times);
+      console.log(big);
     });
-    
+  }
+  else{
+    $("#spin").click(function () {
+      $("#winnings").text("");
+      //var extraDegree = [Math.floor(scam[attempts])];
+      var extraDegree = 395;
+
+      //add 1 every click
+      clicks++;
+      attempts++;
+
+      // if (times == 1) {
+      //   extraDegree = 395;
+      // }
+
+      var newDegree = degree * clicks;
+      totalDegree = newDegree + extraDegree;
+
+      /*let's make the spin btn to tilt every
+		time the edge of the section hits 
+		the indicator*/
+      $("#wheel .sec").each(function () {
+        var t = $(this);
+        var noY = 0;
+
+        var c = 0;
+        var n = 700;
+        var interval = setInterval(function () {
+          c++;
+          if (c === n) {
+            clearInterval(interval);
+          }
+
+          var aoY = t.offset().top;
+          $("#txt").html(aoY);
+
+          /*23.7 is the minumum offset number that 
+				each section can get, in a 30 angle degree.
+				So, if the offset reaches 23.7, then we know
+				that it has a 30 degree angle and therefore, 
+				exactly aligned with the spin btn*/
+          if (aoY < 23.89) {
+            $("#spin").addClass("spin");
+            setTimeout(function () {
+              $("#spin").removeClass("spin");
+            }, 100);
+          }
+        }, 10);
+
+        $("#inner-wheel").css({
+          transform: "rotate(" + totalDegree + "deg)",
+        });
+
+        $("#inner-wheel").on(
+          "transitionend webkitTransitionEnd oTransitionEnd",
+          function () {
+            $("#winnings").text("BIGGEST PRIZE: LANDYARDDDDD");
+              // switch (values[attempts - 1]) {
+              //   case "red":
+              //     $("#winnings").text("Better luck next time");
+              //     // $("#winnings").text("BIGGEST PRIZE: LANDYARDDDDD");
+              //     break;
+              //   case "yellow":
+              //     $("#winnings").text("Wow you win a loofah!");
+              //     break;
+              //   case "blue":
+              //     $("#winnings").text("Greenmart's pen is waiting for you!!!");
+              //     break;
+              //   case "green":
+              //     $("#winnings").text("You have one more spin");
+              //     break;
+              //   case "dark blue":
+              //     $("#winnings").text(
+              //       "Arg, I also want a cup holder. Take yours"
+              //     );
+              //     break;
+              //   default:
+              //     $("#winnings").text("Wow you win loofah!"); // fix bug
+              // }
+            }
+        );
+
+        noY = t.offset().top;
+        
+      });
+    });
   }
 }); //DOCUMENT READY
